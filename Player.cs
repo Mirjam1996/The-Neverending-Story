1a661db5c22050dc69d0f36f03f2d952d561d638
Mirjam1996 <40367077+Mirjam1996@users.noreply.github.com>
2018-08-22 21:49:23 +0200

    Add files via upload
    PlayerBehaviour habe ich selber programmiert, während ich Player aus einem YouTube Video habe, beides ist noch nicht perfekt und muss noch überarbeitet werden. Das sollte aber auch noch besser aussehen, wenn wir die Animationen von dem Fuchs mit drin haben. Das habe ich jetzt aber an Ende der Prioritätskette gestellt erstmal. CameraBehaviour Folgt den Bewegungen des Charakters, was ich als die beste Möglichkeit der Kamerabewegung angesehen habe, aber darüber kann man ja nochmal reden. Auch dies habe aus einem YouTube Video. Wir sollten da auf jeden Fall noch Änderungen in den Benennungen vornehmen und Kommentare einfügen.

---
+++ Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Animator _animator;
+
+    private CharacterController _characterController;
+
+    public float Speed = 5.0f;
+
+    public float RotationSpeed = 240.0f;
+
+    private float Gravity = 20.0f;
+
+    private Vector3 _moveDir = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Input for axis
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // Calculate the forward vector
+        Vector3 camForward_Dir = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 move = v * camForward_Dir + h * Camera.main.transform.right;
+
+        if (move.magnitude > 1f) move.Normalize();
+
+        // Calculate the rotation for the player
+        move = transform.InverseTransformDirection(move);
+
+        // Get Euler angles
+        float turnAmount = Mathf.Atan2(move.x, move.z);
+
+        transform.Rotate(0, turnAmount * RotationSpeed * Time.deltaTime, 0);
+
+        if (_characterController.isGrounded)
+        {
+            _animator.SetBool("run", move.magnitude > 0);
+
+            _moveDir = transform.forward * move.magnitude;
+
+            _moveDir *= Speed;
+
+        }
+
+        _moveDir.y -= Gravity * Time.deltaTime;
+
+        _characterController.Move(_moveDir * Time.deltaTime);
+    }
+}
---
+++ PlayerBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    
+    public int length;
+    public float speed = 0.3f;
+    private float timer = 0f;
+    private float winkel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void Forward(int length,float speed){
+        transform.Translate(length * speed, 0f, 0f);
+        length += 1;
+    }
+    void Backwards(int length, float speed)
+    {
+        transform.Translate(-length * speed, 0f, 0f);
+        length += 1;
+    }
+    void Right(int length, float speed)
+    {
+        transform.Translate(0f, 0f, length * speed);
+        length += 1;
+    }
+    void Left(int length, float speed)
+    {
+        transform.Translate(0f, 0f, -length * speed);
+        length += 1;
+    }
+    void Turn(float winkel){
+        transform.Rotate(0f, winkel, 0f);
+    }
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        if (timer >= 1.0f)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Forward(1, 1.0f);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Backwards(1, 1.0f);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if(Input.GetKeyDown(KeyCode.RightArrow)){
+                    Turn(90.0f); 
+                } 
+                Right(1, 1.0f);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Left(1, 1.0f);
+            }
+        }
+	}
+}
