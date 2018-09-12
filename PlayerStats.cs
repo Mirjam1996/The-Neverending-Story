7af20e5d235221b9bf78b5279e44a0f50d588953
FabianHastenpflug <40369491+FabianHastenpflug@users.noreply.github.com>
2018-09-12 13:49:41 +0200

    Add files via upload
    C#-Skript um die Lebens- und Hungerwerte des Fuchses zu berechnen und an die GUI zu übergeben.
    Screenshot des ersten GUI-Entwurfes

---
+++ PlayerStats.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //wichtig
+
+public class PlayerStats : MonoBehaviour { //Skript wird dem Fuchs zugeordnet.
+
+    //folgende Werte lassen sich in Unity einstellen:
+    public float Health;
+    public float healtOverTime;
+
+    public float Hunger;
+    public float hungerOverTime;
+
+    //Zwei Slider-Elemente, die mit den Slider der GUI verbunden werden.
+    public Slider HealthBar;
+    public Slider HungerBar;
+
+    //Ab diesem Hungerwert verliert man Leben.
+    public float minAmount = 5f;
+
+    Rigidbody myBody;
+
+	
+	private void Start () {
+        myBody = GetComponent<Rigidbody>();
+        //Man startet mit vollem Leben und voller Hungeranzeige.
+        HealthBar.maxValue = Health;
+        HungerBar.maxValue = Hunger;
+        
+        updateUI();
+	}
+	
+	private void Update () {
+        //Wird ausgelagert in:
+        CalculateValues();
+	}
+
+    private void CalculateValues() {
+        //Hungeranzeige reduziert sich (Stärke festlegbar).
+        Hunger -= hungerOverTime * hungerOverTime.deltaTime;
+
+        //Sobald Hunger den Wert 'minAmount' unterschritten hat, reduziert sich auch die Lebensanzeige.
+        if(Hunger <= minAmount) {
+            Health -= healthOverTime * hungerOverTime.deltaTime;
+        }
+
+        //Wenn nix Gesundheit --> nix fröhlich weiter rumlaufen.
+        if(Health <= 0) {
+            printf("PLAYER HAS DIED");
+        }
+    }
+
+    //GUI wird geupdatet.
+    private void updateUI() {
+        Health = Mathf.Clamp(Health, 0, 100f);
+        Hunger = Mathf.Clamp(Hunger, 0, 100f);
+
+        //Zuordnung der neuen Werte.
+        HealthBar.value = Health;
+        HungerBar.value = Hunger;
+    }
+
+    //Schadenmenge ('amount') wird vom aktuellen Leben abgezogen.
+    public void TakeDamage(float amount) {
+        Health -= amount;
+        updateUI();
+    }
+}
