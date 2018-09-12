920088f55d4ff5a45079b0d5cffd245cb15a3896
MarkLinke <40383379+MarkLinke@users.noreply.github.com>
2018-09-12 17:25:03 +0200

    Add files via upload
    Skripte Fuchssteuerung und Kamera

---
+++ CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public float lookSmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 2, -4);
+    public float xTilt = 15;
+    public float zTilt = 10;
+
+    Vector3 destination = Vector3.zero;
+    CharacterController charController;
+
+    float rotateVelocity = 0;
+
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t)
+    {
+        target = t;
+        if (target != null)
+        {
+            if (target.GetComponent<CharacterController>())
+            {
+                charController = target.GetComponent<CharacterController>();
+            }
+            else
+                Debug.LogError("The camera's target needs a character controller.");
+        }
+        else
+            Debug.LogError("Your camera needs a target.");
+    }
+
+
+    void LateUpdate()
+    {
+        //moving & rotating
+        MoveTotarget();
+        LookAtTarget();
+    }
+
+    void MoveTotarget()
+    {
+        destination = charController.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+
+    }
+
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVelocity, lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+}
---
+++ CharacterController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float inputDelay = 0.1f;
+    public float forwardVelocity = 14;
+    public float rotateVelocity = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The character needs a rigidbody.");
+
+        forwardInput = turnInput = 0;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            forwardVelocity = 21;
+        }
+        else
+            forwardVelocity = 12;
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        {
+            //move
+            rBody.velocity = transform.forward * forwardInput * forwardVelocity;
+        }
+        else
+        {
+            //zero.Velocity
+            rBody.velocity = Vector3.zero;
+        }
+    }
+
+    void Turn()
+    {
+        targetRotation *= Quaternion.AngleAxis(rotateVelocity * turnInput * Time.deltaTime, Vector3.up);
+        transform.rotation = targetRotation;
+
+    }
+}
