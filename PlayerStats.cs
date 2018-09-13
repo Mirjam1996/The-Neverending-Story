badc97aef00729c37d47c28335a6b05df3b9cf3f
Mirjam1996 <40367077+Mirjam1996@users.noreply.github.com>
2018-09-13 19:10:27 +0200

    Update PlayerStats.cs

--- PlayerStats.cs
+++ PlayerStats.cs
@@ -1,69 +1,80 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; //wichtig
+using UnityEngine.UI;
 
-public class PlayerStats : MonoBehaviour { //Skript wird dem Fuchs zugeordnet.
+public class PlayerStats : MonoBehaviour
+{
 
-    //folgende Werte lassen sich in Unity einstellen:
     public float Health;
-    public float healtOverTime;
+    public float healthOverTimer;
+
+
 
     public float Hunger;
     public float hungerOverTime;
 
-    //Zwei Slider-Elemente, die mit den Slider der GUI verbunden werden.
+   
+
+
+
     public Slider HealthBar;
+
     public Slider HungerBar;
 
-    //Ab diesem Hungerwert verliert man Leben.
+
     public float minAmount = 5f;
+
 
     Rigidbody myBody;
 
-	
-	private void Start () {
+    private void Start()
+    {
         myBody = GetComponent<Rigidbody>();
-        //Man startet mit vollem Leben und voller Hungeranzeige.
         HealthBar.maxValue = Health;
-        HungerBar.maxValue = Hunger;
-        
-        updateUI();
-	}
-	
-	private void Update () {
-        //Wird ausgelagert in:
-        CalculateValues();
-	}
 
-    private void CalculateValues() {
-        //Hungeranzeige reduziert sich (Stärke festlegbar).
+        HungerBar.maxValue = Hunger;
+
+
+        updateUI();
+    }
+
+    private void Update()
+    {
+        CalculateValues();
+    }
+
+    private void CalculateValues()
+    {
         Hunger -= hungerOverTime * Time.deltaTime;
 
-        //Sobald Hunger den Wert 'minAmount' unterschritten hat, reduziert sich auch die Lebensanzeige.
-        if(Hunger <= minAmount) {
-            Health -= healthOverTime * Time.deltaTime;
-        }
 
-        //Wenn nix Gesundheit --> nix fröhlich weiter rumlaufen.
-        if(Health <= 0) {
+        if (Health <= 0)
+        {
             print("PLAYER HAS DIED");
         }
-    }
 
-    //GUI wird geupdatet.
-    private void updateUI() {
-        Health = Mathf.Clamp(Health, 0, 100f);
-        Hunger = Mathf.Clamp(Hunger, 0, 100f);
-
-        //Zuordnung der neuen Werte.
-        HealthBar.value = Health;
-        HungerBar.value = Hunger;
-    }
-
-    //Schadenmenge ('amount') wird vom aktuellen Leben abgezogen.
-    public void TakeDamage(float amount) {
-        Health -= amount;
         updateUI();
     }
+
+    private void updateUI()
+    {
+        Health = Mathf.Clamp(Health, 0, 100f);
+
+        Hunger = Mathf.Clamp(Hunger, 0, 100f);
+       
+
+        HealthBar.value = Health;
+
+        HungerBar.value = Hunger;
+       
+    }
+
+    public void TakeDamage(float amnt)
+    {
+        Health -= amnt;
+        updateUI();
+    }
+
+    //end o class
 }
